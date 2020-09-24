8b8bf881af00c1b157c74bc3387efba64461314e
Anastasya-art <56293443+Anastasya-art@users.noreply.github.com>
2020-09-24 11:06:49 +0300

    Update dz1.c

--- dz1.c
+++ dz1.c
@@ -20,7 +20,96 @@ int pop(locate_t **head) {
      (*head) = (*head)->next;
 
      free(prev);
+     return loc;#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+typedef struct locate{
+    int loc;
+    struct locate * next;
+} locate_t;
+
+int pop(locate_t **head) {
+    locate_t * prev = NULL;
+    int loc;
+
+    if (head == NULL) {
+        exit(-1);
+     }  
+
+     prev = (*head);
+     loc = prev->loc;
+     (*head) = (*head)->next;
+
+     free(prev);
      return loc;
+}
+
+void push(locate_t **head, int loc){
+     locate_t *tmp = (locate_t*) malloc(sizeof(locate_t));
+     tmp->loc = loc;
+     tmp->next = (*head);
+     (*head) = tmp;
+}
+
+void autofill(locate_t **head){
+    
+    int *arr; // указатель на массив
+    int tmp_size, size;
+    
+    printf("Введите размер кол-во элементов:");
+    scanf("%d", &size);
+
+// Выделение памяти
+    arr = (int*)malloc(size * sizeof(int));
+
+// Ввод элементов массива
+    for (tmp_size = 0; tmp_size<size; tmp_size++){
+        
+        printf("%d элемент = ", tmp_size + 1);
+        scanf("%d", &arr[tmp_size]);
+    } 
+
+    size_t i = size - 1;
+
+    if (arr == NULL || size == 0){
+    return;
+    }
+
+    do {
+    push(head, arr[i]);
+
+    } while(i--!=0);
+    
+    free(arr);
+}
+
+void print(locate_t *head){
+    while (head) {
+        printf("%d  ", head->loc);
+        head = head->next;
+    }
+printf("\n");
+
+}
+
+int main(void){
+
+    //Добавление массива
+    locate_t * head = NULL;
+    autofill(&head);
+    print(head);
+
+
+    // Удаление элемента
+    pop(&head);
+
+    // Вывод на экран
+    printf("Числа структуры:\n");
+    print(head);
+
+    return 0;
+}
 }
 
 void push(locate_t **head, int data){
@@ -69,7 +158,6 @@ printf("\n");
 
 
 int main(void){
-    setlocale(LC_ALL, "Rus");
 
     //Добавление массива
     locate_t * head = NULL;
